479da4ca1707663ba2da1b99570309144948de2f
ec2-user <ec2-user@ip-172-31-16-100.ap-southeast-2.compute.internal>
2014-04-19 22:33:15 +1000

    update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,15 @@
 #create user webchecker and grant privilege 
 create user 'webchecker'@'%' identified by '****';
 grant all on monitor.* to 'webchecker'@'localhost';
+
+create table DNSchecklog
+( timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  hostname varchar(100),
+  ip varchar(50),
+  status ENUM('production','contingency','other','none'));
+  
+create table DNSRecords
+( hostname varchar(100) not null,
+  productionIP varchar(50),
+  contingencyIP varchar(50)
+  )
