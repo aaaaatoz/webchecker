43f208be112dfd89fedb63d945bab80faa1fc651
ec2-user <ec2-user@ip-172-31-16-100.ap-southeast-2.compute.internal>
2014-04-21 22:01:59 +1000

    put the result into database

--- getipbyhostname.py
+++ getipbyhostname.py
@@ -16,6 +16,21 @@ def getHOSTfromdb():
         		con.close()
 		return records
 
+def writeDNSrecor(hostname,ip,status):
+	try:
+		con = mdb.connect('localhost', 'webchecker', '', 'monitor');
+		cur = con.cursor()
+		#cur.execute("select * from DNSRecords")
+		print hostname,ip,status
+		insertstring = "insert into DNSchecklog(hostname,ip,status) values('%s','%s' ,'%s')" %(hostname,ip,status)
+		print insertstring
+		cur.execute(insertstring)
+	except mdb.Error, e:
+		print "here"
+    		sys.exit(1)
+	finally:
+    		if con:
+        		con.close()
 
 def getIPByHostname(host):
 	try:
@@ -34,11 +49,16 @@ if __name__=='__main__':
 		time = str (datetime.datetime.now())
 		if currentIP == checkinfo[1]:
 			log.write(time+"\t"+"%s \t\tRunning on Production \t%s\n" %(checkinfo[0],currentIP))
+			status = "production"
 		elif currentIP == checkinfo[2]:
 			log.write(time+"\t"+"%s \t\tRunning on Contingency \t%s\n" %(checkinfo[0],currentIP))
+			status = "contingency"
 		else:
 			log.write(time+"\t"+"%s \t\tRunning on \t%s\n" %(checkinfo[0],currentIP))
+			currentIP = "None"
+			status = "none"
 		log.flush()
+		writeDNSrecor(checkinfo[0],currentIP,status)
 	log.close()
 		
 			
