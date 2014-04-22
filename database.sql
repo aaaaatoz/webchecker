e7df87eccc853822a3defbec7bae7db1b4411c47
aaaaatoz <rafa.xu.au@gmail.com>
2014-04-22 21:08:31 +1000

    update database.sql

--- database.sql
+++ database.sql
@@ -19,3 +19,17 @@ create table DNSRecords
  insert into DNSRecords values('www.myplates.com.au','163.189.7.166','163.189.217.166');
  insert into DNSRecords values('www.scats.com.au','163.189.7.167','163.189.217.167');
  insert into DNSRecords values('www.sydneymotorways.com.au','163.189.7.162','163.189.217.162');
+
+create table URLRecords (url varchar(200), checked boolean, keywords varchar(20), outagekeyword varchar(20));
+
+create table URLchecklog
+( timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  url varchar(200),
+  returncode varchar(10),
+  status ENUM('production','outage','error','none'));
+
+ insert into URLRecords(url,checked,keywords,outagekeyword) values('http://www.rms.nsw.gov.au',true,'Roads and Maritime Service','outage');
+ insert into URLRecords(url,checked,keywords,outagekeyword) values('https://myrta.com/myEToll/',false,'Roads and Maritime Service','outage');
+ insert into URLRecords(url,checked,keywords,outagekeyword) values('http://www.myplates.com.au',true,'Roads and Maritime Service','outage');
+ insert into URLRecords(url,checked,keywords,outagekeyword) values('http://www.scats.com.au',true,'scats','outage');
+ insert into URLRecords(url,checked,keywords,outagekeyword) values('http://sydneymotorways.com',true,'Roads and Maritime Service','outage');
